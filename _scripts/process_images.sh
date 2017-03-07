01ef6e466f64307fc283794d594cfcdee694a8c3
Zach Kirsch <zachkirsch@gmail.com>
2017-03-07 20:17:24 +0100

    for creating smaller versions and thumbs of images

---
+++ _scripts/process_images.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+img_root="/Users/Zach/Documents/Developer/personal_site_2/assets/portfolio/img"
+origs="${img_root}/origs"
+thumbs="${img_root}/thumbs"
+fulls="${img_root}/fulls"
+
+rm -f "${thumbs}"/*
+rm -f "${fulls}"/*
+
+for img in "${origs}"/*
+do
+    date=$(exiftool -s -s -s -d '%Y-%m-%d-%H-%M-%S' -DateTimeOriginal "$img") \
+    && filename="${date}.jpg" \
+    && convert "$img" -auto-orient -thumbnail 360x225 "${thumbs}/${filename}" \
+    && convert "$img" -auto-orient -size 1200x750 "${fulls}/${filename}" \
+    && echo "☑️️  $filename"
+done
