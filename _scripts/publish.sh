1a8e5032bc178ef3120a5168298150e42aa0cdf0
Zach Kirsch <zachkirsch@gmail.com>
2017-03-08 22:17:31 +0100

    building for production env when publishing

--- _scripts/publish.sh
+++ _scripts/publish.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 cd /Users/Zach/Documents/Developer/personal_site_2/
-jekyll build
+JEKYLL_ENV=production jekyll build
 cd /Users/Zach/Documents/Developer/personal_site_2/_site
 git add .
 git commit -m "publishing site"
