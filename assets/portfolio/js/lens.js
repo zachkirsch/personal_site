5663588f00a4f1d9d41bbbf40c31a3c275fdff07
Zach Kirsch <zachkirsch@gmail.com>
2017-03-09 18:35:58 +0100

    4 thumbs per row instead of 3

--- assets/portfolio/js/lens.js
+++ assets/portfolio/js/lens.js
@@ -22,7 +22,7 @@ var main = (function($) { var _ = {
       layoutDuration: 0,
 
     // Thumbnails per "row" (must match "misc.thumbnails-per-row" in _vars.scss).
-      thumbnailsPerRow: 3,
+      thumbnailsPerRow: 4,
 
     // Side of main wrapper (must match "misc.main-side" in _vars.scss).
       mainSide: 'right'
