c3d56529d1b851882798740acb0ee53d56c280eb
Zach Kirsch <zachkirsch@gmail.com>
2017-03-28 19:08:45 +0200

    using gnu parallel to process images 5 at a time

--- _scripts/process_images.sh
+++ _scripts/process_images.sh
@@ -1,28 +1,13 @@
 #!/usr/bin/env bash
 #
 # process original (high res) images into thumbnails and "full size", which are
-# still good quality but manageably sized.
+# still good quality but manageably sized. Uses GNU Parallel to process 5 at a
+# time.
 #
-# TODO: this converts every picture in the background simultaneously, so while
-# it's running it hogs CPU like crazy.
+# To use: run the script, and the export the high-res photos into the "origs"
+# folder
 #
 # Zach Kirsch | March 2017
-
-process_image() {
-    img="$1"
-    date=$(exiftool -s -s -s -d '%Y-%m-%d-%H-%M-%S' -DateTimeOriginal "$img")
-    if [[ ! -z "${date}" ]]; then
-        filename="${date}.jpg"
-        convert "$img" -auto-orient -quality 1 -thumbnail 2000x2000 "${thumbs}/${filename}"
-        convert "$img" -auto-orient -resize 2000x2000 "${fulls}/${filename}"
-        echo "resized: $filename"
-    else
-        echo "Error: could not determine date of image: $img"
-    fi
-}
-
-set -e # Exit on error
-set -m # Enable Job Control
 
 site_root="/Users/Zach/Documents/Developer/personal_site_3"
 portfolio_root="${site_root}/assets/portfolio"
@@ -32,21 +17,33 @@ origs="${img_root}/origs"
 thumbs="${img_root}/thumbs"
 fulls="${img_root}/fulls"
 
-# rm -rf "$fulls" "$thumbs" "$origs"
-# mkdir "$fulls" "$thumbs" "$origs"
+process_image() {
+    img="$1"
+    thumbs="$2"
+    fulls="$3"
+    date=$(exiftool -s -s -s -d '%Y-%m-%d-%H-%M-%S' -DateTimeOriginal "$img")
+    if [[ ! -z "${date}" ]]; then
+        filename="${date}.jpg"
+        convert "$img" -auto-orient -quality 1 -thumbnail 2000x2000 "${thumbs}/${filename}"
+        convert "$img" -auto-orient -resize 2000x2000 "${fulls}/${filename}"
+        # echo "resized: $filename"
+    else
+        echo "Error: could not determine date of image: $img"
+    fi
+}
 
-echo "Export images to: $origs"
-printf "Then press any key to continue...\n"
+set -e # Exit on error
+
+rm -rf "$fulls" "$thumbs" "$origs"
+mkdir "$fulls" "$thumbs" "$origs"
+
+printf "Export images to: $origs. Then press any key to continue...\n"
 read -n 1 reply </dev/tty
 
-for img in "${origs}"/*
-do
-    process_image "$img" &
-done
-
-wait
+export -f process_image
+parallel --bar -j 5 "process_image {} ${thumbs} ${fulls}" ::: "${origs}"/*
 
 cd "$img_root"
 mv "$fulls" portfolio/
-zip -r portfolio.zip portfolio
+zip -qr portfolio.zip portfolio
 mv portfolio/ "$fulls"
