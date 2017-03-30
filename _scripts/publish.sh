9edffde2975e03aa32494a7b5ded5dc087fda400
Zach Kirsch <zachkirsch@gmail.com>
2017-03-30 19:00:29 +0200

    using 'bundle exec' to suppress version warnings

--- _scripts/publish.sh
+++ _scripts/publish.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 cd /Users/Zach/Documents/Developer/personal_site_3/
-JEKYLL_ENV=production jekyll build
+JEKYLL_ENV=production bundle exec jekyll build
 cd /Users/Zach/Documents/Developer/personal_site_3/_site
 git add .
 git commit -m "publishing site"
