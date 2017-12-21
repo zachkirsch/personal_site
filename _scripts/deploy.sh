eda5a7a4182832f423bdfd992308d21a48bbf0b4
Zach Kirsch <zachkirsch@gmail.com>
2017-12-21 11:30:25 -0500

    incorporate 150 site

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
 
-cd /Users/Zach/Documents/Developer/personal_site/personal_site
+# build jekyll site
+cd /Users/Zach/Documents/Developer/personal_site/jekyll_site
 JEKYLL_ENV=production bundle exec jekyll build
-cd /Users/Zach/Documents/Developer/personal_site/personal_site/_site
+
+# copy comp 150 react project as well
+cd /Users/Zach/Documents/Developer/personal_site/comp-150-alg-project
+yarn build
+cp -r build /Users/Zach/Documents/Developer/personal_site/jekyll_site/_site/compression
+
+cd /Users/Zach/Documents/Developer/personal_site/jekyll_site/_site
 git add .
 git commit -m "publishing site"
 git push
