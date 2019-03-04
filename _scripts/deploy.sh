83a3909a9ab502d3e057f3c2dd7da5d6fe2e2e51
Zach Kirsch <zkirsch@palantir.com>
2019-03-03 22:28:42 -0500

    remove resume

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -3,7 +3,7 @@
 set -e
 
 # directories
-site_root=/Users/Zach/Documents/Developer/personal_site
+site_root=/Users/zkirsch/Dropbox/Documents/Developer/archive/personal_site
 jekyll_source=$site_root/jekyll_site
 jekyll_static_site=$jekyll_source/_site
 comp_150_source=$site_root/comp-150-alg-project
@@ -14,9 +14,9 @@ cd $site_root/jekyll_site
 JEKYLL_ENV=production bundle exec jekyll build
 
 # copy comp 150 react project as well
-cd $comp_150_source
-yarn build
-cp -r build $comp_150_dest
+# cd $comp_150_source
+# yarn build
+# cp -r build $comp_150_dest
 
 # push static site to Github
 cd $jekyll_static_site
